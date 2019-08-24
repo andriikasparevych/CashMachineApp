61dfe1802cf40948d19d9b2046b9b0b4b2e9ba24
Andrew <andrewkasparevich@gmail.com>
2019-08-24 17:29:58 +0200

    Add ArrayUtils for sorting

---
+++ CashMachineApp.Tests/ArrayUtilsTests.cs
@@ -0,0 +1,32 @@
+using System;
+using CashMachineApp.Utils;
+using Xunit;
+
+namespace CashMachineApp.Tests
+{
+    public class ArrayUtilsTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { }, new int[] { })]
+        [InlineData(new[] {1,2,3,4}, new[] {4,3,2,1})]
+        [InlineData(new[] {-1,2,-3,4}, new[] {4,2,-1,-3})]
+        [InlineData(new[] {20, 50, 10}, new[] {50,20,10})]
+        [InlineData(new[] {8,8,8,3,1,2,4,5}, new[] {8,8,8,5,4,3,2,1})]
+        public void SortDesc_ValidInputs_ShouldReturn_Expected(int[] input, int[] expected)
+        {
+            ArrayUtils.SortDesc(input);
+
+            Assert.Equal(expected, input);
+        }
+
+        [Fact]
+        public void SortDesc_InputIsNull_ShoudThrow_ArgumentNullException()
+        {
+
+            var act = new Action(() => ArrayUtils.SortDesc(null));
+
+            Assert.Throws<ArgumentNullException>(act);
+        }
+    }
+}
---
+++ CashMachineApp/Utils/ArrayUtils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashMachineApp.Utils
+{
+    public static class ArrayUtils
+    {
+
+        public static void SortDesc(int[] arr)
+        {
+            if (arr == null) throw new ArgumentNullException(nameof(arr));
+            QuickSortDesc(arr, 0, arr.Length - 1);
+        }
+
+        private static void QuickSortDesc(int[] arr, int left, int right)
+        {
+            if (left >= right) return;
+
+            var partIndex = Partition(arr, left, right);
+
+            QuickSortDesc(arr, partIndex + 1, right);
+            QuickSortDesc(arr, left, partIndex - 1);
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[right];
+
+            var partIndex = left - 1;
+
+            for (var i = left; i < right; i++)
+            {
+                if (arr[i] >= pivot)
+                {
+                    partIndex++;
+                    Swap(arr, partIndex, i);
+                }
+            }
+
+            partIndex++;
+            Swap(arr, right, partIndex);
+            return partIndex;
+        }
+
+        private static void Swap(int[] arr, int first, int second)
+        {
+            var tmp = arr[second];
+            arr[second] = arr[first];
+            arr[first] = tmp;
+        }
+
+    }
+}
