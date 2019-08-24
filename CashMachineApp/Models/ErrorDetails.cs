6be32f33870834013c03b8e77931a057ec97ff84
Andrew <andrewkasparevich@gmail.com>
2019-08-24 20:00:25 +0200

    Add exception handling

---
+++ CashMachineApp/Models/ErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace CashMachineApp.Models
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
--- CashMachineApp/Startup.cs
+++ CashMachineApp/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Net;
+using CashMachineApp.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashMachineApp
@@ -11,6 +15,8 @@ namespace CashMachineApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddTransient<ICashMachineService, CashMachineService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -20,6 +26,27 @@ namespace CashMachineApp
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message
+                        }.ToJson());
+                    }
+                });
+
+            });
 
             app.UseMvc(routes =>
             {
